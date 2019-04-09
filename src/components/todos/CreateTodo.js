d8c135546f37a88bf88adb42ba07bf19e3f89873
carly <carly@noukod.com>
2019-04-09 13:09:07 +0000

    Automatically backed up by Learn

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react'
  
 class CreateTodo extends Component {
-  render() {
-    return(
-      <div>
-        <form>
-          <p>
-            <label>add todo</label>
-            <input type="text" />
-          </p>
-          <input type="submit" />
-        </form>
-      </div>
-    );
-  }
+ render(){
+  return(
+    <div>
+      <form>
+        <p>
+          <label>add todo</label>
+          <input type="text" onChange={(event) => this.handleChange(event)}/>
+        </p>
+        <input type="submit" />
+      </form>
+    </div>
+  );
+}
 };
  
 export default CreateTodo;
